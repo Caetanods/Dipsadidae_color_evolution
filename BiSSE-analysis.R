0df7a53c904c6baa12e22467ab841c11790b7cb1
Caetanods <caetanods1@gmail.com>
2014-09-30 19:37:25 -0700

    Continuing coda analysis

--- BiSSE-analysis.R
+++ BiSSE-analysis.R
@@ -30,12 +30,15 @@ load("./results_bisse.RData")
 
 ###########################
 
+###########################
 ## Check for convergence using the coda package:
-
+## Will also try to use use bonsai here.
 library(coda)
 
 ## Create directory to save plots:
 dir.create("./mcmc_coda_plots")
+
+## First the constrained results:
 
 ## Trace plots
 dim(mcmc.onerate[[1]][[1]])[1]
@@ -53,12 +56,12 @@ for(i in 1:runs){
 ## Taking out a 25% burn-in:
 burn <- list()
 for(i in 1:runs){
-    burn[[i]] <- mcmc.onerate[[i]][[1]][(dim(res[[i]][[1]])[1]*25/100):dim(res[[i]][[1]])[1],]
+    burn[[i]] <- mcmc.onerate[[i]][[1]][(dim(mcmc.onerate[[i]][[1]])[1]*25/100):dim(mcmc.onerate[[i]][[1]])[1],]
 }
 
-lb <- paste("./mcmc_coda_plots/lambda_",seq(1,runs),".pdf", sep="")
-mu <- paste("./mcmc_coda_plots/mu_",seq(1,runs),".pdf", sep="")
-mk <- paste("./mcmc_coda_plots/markov_",seq(1,runs),".pdf", sep="")
+lb <- paste("./mcmc_coda_plots/lambda_cons_",seq(1,runs),".pdf", sep="")
+mu <- paste("./mcmc_coda_plots/mu_cons_",seq(1,runs),".pdf", sep="")
+mk <- paste("./mcmc_coda_plots/markov_cons_",seq(1,runs),".pdf", sep="")
 
 for(i in 1:runs){
     pdf(file = lb[i])
@@ -74,21 +77,64 @@ for(i in 1:runs){
     dev.off()
 }
 
-## Now need to run a coda analysis to make sure that this is the converged distribution.
+## The results of the full model:
 
-sm <- list()
-hei <- list()
-for(i in 1:10){
-    res <- res.cr[[i]][[1]]
+## Trace plots
+dim(mcmc.tworate[[1]][[1]])[1]
+index <- round(seq(1, dim(mcmc.tworate[[1]][[1]])[1], length.out = 500))
+runs <- length(mcmc.onerate)
+
+ng <- paste("./mcmc_coda_plots/trace_full_",seq(1,runs),".pdf", sep="")
+for(i in 1:runs){
+    pdf(ng[i])
+    chain <- mcmc.tworate[[i]][[1]]
+    plot(chain$p[index], type = "l")
+    dev.off()
+}
+
+## Taking out a 25% burn-in:
+burn <- list()
+for(i in 1:runs){
+    burn[[i]] <- mcmc.tworate[[i]][[1]][(dim(mcmc.tworate[[i]][[1]])[1]*25/100):dim(mcmc.tworate[[i]][[1]])[1],]
+}
+
+lb <- paste("./mcmc_coda_plots/lambda_full_",seq(1,runs),".pdf", sep="")
+mu <- paste("./mcmc_coda_plots/mu_full_",seq(1,runs),".pdf", sep="")
+mk <- paste("./mcmc_coda_plots/markov_full_",seq(1,runs),".pdf", sep="")
+
+for(i in 1:runs){
+    pdf(file = lb[i])
+    profiles.plot(burn[[i]][c("lambda0")], col.line = c("yellow"))
+    dev.off()
+    
+    pdf(file = mu[i])
+    profiles.plot(burn[[i]][c("mu1")], col.line = c("yellow"))
+    dev.off()
+
+    pdf(file = mk[i])
+    profiles.plot(burn[[i]][c("q01","q10")], col.line = c("yellow","blue"), legend.pos = "topright")
+    dev.off()
+}
+
+############################
+## Run diagnostic for convergence.
+
+sm.cons <- list()
+sm.full <- list()
+hei.cons <- list()
+hei.full <- list()
+for(i in 1:runs){
+    res <- mcmc.onerate[[i]][[1]]
     mc <- res[,c(-1,-8)]
     mc.dat <- as.mcmc(mc)
     sm[[i]] <- summary(mc.dat)
     hei[[i]] <- heidel.diag(mc.dat)
 }
 
-hei[[10]]
-## Good! We are in stationarity.
+## Check the results of the Heidelberger and Welch's convergence diagnostic for each run:
+hei[[1]]
 
+################################
 ## Now the combined posterior with half burn in for all the 10 trees:
 
 burn.comb <- list()
@@ -102,36 +148,3 @@ comb.one.rate <- comb
 save(comb.one.rate, file = "combine.one.rate.RData")
 
 ## write.table(comb, file = "Comb.posterior.bisse.cr.txt", sep = ",")
-
-## Now the graphs for the combined posterior:
-
-pdf(file = "comb.lambda.pdf")
-profiles.plot(comb[c("lambda0")], col.line = c("blue"))
-dev.off()
-
-pdf(file = "comb.mu.pdf")
-profiles.plot(comb[c("mu1")], col.line = c("blue"))
-dev.off()
-
-pdf(file = "comb.markov.pdf")
-profiles.plot(comb[c("q01","q10")], col.line = c("yellow","blue"), legend.pos = "topright")
-dev.off()
-
-## pdf(file = "comb.net_diver.pdf")
-## profiles.plot(comb[c("lambda0","lambda1")] - comb[c("mu0","mu1")], col.line = c("yellow","blue")
-##               , legend.pos = "topright")
-## dev.off()
-
-## The results are very similar. Are the trees the same trees?
-## First check only the topology:
-dt <- matrix(nrow = 10, ncol = 10)
-rownames(dt) <- seq(1:10)
-colnames(dt) <- seq(1:10)
-
-for(i in 1:10){
-    for(j in 1:10){
-        dt[i,j] <- dist.topo(tree.gen[[i]],tree.gen[[j]])
-    }
-}
-
-plot(tree.gen[[3]])

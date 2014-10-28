138c1db2acada048ca5cd68a78ce5f4991e0ff0d
Caetanods <caetanods1@gmail.com>
2014-10-28 11:45:15 -0700

    Fix run.bisse.split function.

--- functions/analysis.R
+++ functions/analysis.R
@@ -57,46 +57,50 @@ run.bisse.split <- function(tree, st, unres, nodes, tun.steps, chain.steps, cons
     tree <- multi2di(tree)
     mmm <- match(tree$tip.label, names(st))
 
-	## Prepare the split model:
-	split.t <- rep("Inf", times = length(nodes))
-    lik <- make.bisse.split(tree, st[mmm], unresolved = unres, nodes = nodes, split.t = split.t)
-	
-	if(constrain == "TRUE"){
-		w.init <- rep(1,4)
-		lik <- constrain(lik, lambda1~lambda0, mu1~mu0)
-		print("Constrained model.")
-	} else {
-		w.init <- rep(1,6) 
-		print("Full model.")
-	}
+    ## Prepare the split model:
+    lik <- make.bisse.split(tree, st[mmm], unresolved = unres, nodes = nodes, split.t = "Inf")
 
-	## Flag:
-	print("Start analysis...")
+	## Note that the number of parameters here double at each split of the model.
+	## Each run of MEDUSA found exactly one split point. -> 12 parameters total.
+    if(constrain == "TRUE"){
+	## w.init = free model minus the number of constrained paramters.
+	w.init <- rep(1,8)
+	lik <- constrain(lik, lambda1.1~lambda0.1, mu1.1~mu0.1, lambda1.2~lambda0.2, mu1.2~mu0.2)
+	print("Constrained model.")
+    } else {
+	w.init <- rep(1,12) 
+	print("Full model.")
+    }
 
+    ## Flag:
+    print("Start analysis...")
+
+	## Need also to change the number of parameters for the start point.
     start <- starting.point.bisse(tree)
-    prior <- make.prior.exponential(1 / 2 * (start[1] - start[3]))
+	start.split <- rep(start, 2)
+	names(start.split) <- argnames(lik)
+    prior <- make.prior.exponential(1 / 2 * (start.split[1] - start.split[3]))
+    fit <- find.mle(lik, start.split[argnames(lik)])
 
-    fit <- find.mle(lik, start[argnames(lik)])
-
-	## Flag:
-	print("ML estimate finished...")
+    ## Flag:
+    print("ML estimate finished...")
 
     tun <- mcmc(lik, fit$par, nsteps = tun.steps, w = w.init, lower = 0, print.every = 0, prior = prior)
 
-	if(constrain == "TRUE"){
-		w <- diff(sapply(tun[2:5], range))
-	} else {
-		w <- diff(sapply(tun[2:7], range))
-	}
+    if(constrain == "TRUE"){
+	w <- diff(sapply(tun[2:9], range))
+    } else {
+	w <- diff(sapply(tun[2:13], range))
+    }
 
-	## Flag:
-	print("MCMC tunning finished...")
-	print("Starting MCMC chain...")
+    ## Flag:
+    print("MCMC tunning finished...")
+    print("Starting MCMC chain...")
 
     run <- mcmc(lik, fit$par, nsteps = chain.steps, w = w, lower = 0, print.every = 0, prior = prior)
 
-	## Flag:
-	print("Done!")
+    ## Flag:
+    print("Done!")
 
     return(list(run, lik, prior))
 }

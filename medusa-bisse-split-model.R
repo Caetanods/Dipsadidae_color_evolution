d31c1b1b6bfc4e6ace24568c85fd7cb9fdbb5056
Caetanods <caetanods1@gmail.com>
2014-10-30 18:47:10 -0700

    Add flag argument to run.bisse.split function calls.

--- medusa-bisse-split-model.R
+++ medusa-bisse-split-model.R
@@ -17,10 +17,10 @@ colnames(single) <- colnames(total) <- c("taxon","n.taxa")
 rich <- rbind(total, single)
 
 ## Constraining the MEDUSA model to find at max two partitions in the phylogenies.
-shifts <- lapply(1:5, FUN = function(x) medusa(tree.genus[[x]], richness = rich, criterion = "aicc", partitions = 2) )
+shifts <- lapply(1:4, FUN = function(x) medusa(tree.genus[[x]], richness = rich, criterion = "aicc", partitions = 2) )
 
 ## Prepare nodes vector for BiSSE.split
-nodes <- sapply(1:5, FUN = function(x) shifts[[x]]$summary$Shift.Node[2])
+nodes <- sapply(1:4, FUN = function(x) shifts[[x]]$summary$Shift.Node[2])
 nodes <- as.numeric(nodes)
 
 ## Prepare states vector for BiSSE
@@ -30,10 +30,10 @@ names(state) <- st[,1]
 ## Making an initial analysis only with 4 trees.
 ## Maybe will need more steps in order to converge.
 tasks <- list(
-    job1 <- function() run.bisse.split(tree = tree.genus[[1]], st = state, unres = unres, nodes[1], tun.steps = 100, chain.steps = 10000, constrain = "FALSE"),
-    job2 <- function() run.bisse.split(tree = tree.genus[[2]], st = state, unres = unres, nodes[2], tun.steps = 100, chain.steps = 10000, constrain = "FALSE"),
-    job3 <- function() run.bisse.split(tree = tree.genus[[3]], st = state, unres = unres, nodes[3], tun.steps = 100, chain.steps = 10000, constrain = "FALSE"),
-    job4 <- function() run.bisse.split(tree = tree.genus[[4]], st = state, unres = unres, nodes[4], tun.steps = 100, chain.steps = 10000, constrain = "FALSE")
+    job1 <- function() run.bisse.split(tree = tree.genus[[1]], st = state, unres = unres, nodes[1], tun.steps = 100, chain.steps = 10000, constrain = "FALSE", flag = "phy_1"),
+    job2 <- function() run.bisse.split(tree = tree.genus[[2]], st = state, unres = unres, nodes[2], tun.steps = 100, chain.steps = 10000, constrain = "FALSE", flag = "phy_2"),
+    job3 <- function() run.bisse.split(tree = tree.genus[[3]], st = state, unres = unres, nodes[3], tun.steps = 100, chain.steps = 10000, constrain = "FALSE", flag = "phy_3"),
+    job4 <- function() run.bisse.split(tree = tree.genus[[4]], st = state, unres = unres, nodes[4], tun.steps = 100, chain.steps = 10000, constrain = "FALSE", flag = "phy_4")
 )
 
 out <- mclapply(tasks, function(f) f(), mc.cores = 4)

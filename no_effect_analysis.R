550cb60fc04a12ee810865c28bd4c844da873d86
Caetanods <caetanods1@gmail.com>
2015-04-01 10:35:14 -0700

    Do MLE instead of the mcmc.

--- no_effect_analysis.R
+++ no_effect_analysis.R
@@ -22,16 +22,18 @@ download.file(url="http://files.figshare.com/1696849/results_100_phylo_bisse.RDa
 
 ## Load results from the BiSSE mcmc for 100 trees.
 load("./mcmc_BiSSE_results/results_100_phylo_bisse.RData")
-rm(mcmc.onerate)
 
 ## Get the mean of the transition rates for each posterior for 100 trees.
-ll <- length(mcmc.tworate)
-mn <- sapply(1:ll, function(x) apply(mcmc.tworate[[x]][[1]][,c(6,7)], 2, mean) )
+## Both results from the full and constrained BiSSE models.
+mn.full <- sapply(1:100, function(x) apply(mcmc.tworate[[x]][[1]][,c(6,7)], 2, mean) )
+mn.cons <- sapply(1:100, function(x) apply(mcmc.onerate[[x]][[1]][,c(3,4)], 2, mean) )
 
 ## Simulate trait data under a Mk2 model with root = 1. Root value derived from ancestral state
 ##     estimate using BiSSE and the empirical data and trees.
-res <- lapply(1:ll, function(x) sim.character(phy[[x]], as.numeric(mn[,x]), x0=1, model = "mk2") )
-sim.null <- t(do.call(rbind, res))
+res.full <- lapply(1:100, function(x) sim.character(phy[[x]], as.numeric(mn.full[,x]), x0=1, model = "mk2") )
+sim.full.null <- t(do.call(rbind, res.full))
+res.cons <- lapply(1:100, function(x) sim.character(phy[[x]], as.numeric(mn.cons[,x]), x0=1, model = "mk2") )
+sim.cons.null <- t(do.call(rbind, res.cons))
 
 ## Drop tips to make tree a clade tree for accounting phylogenetic uncertainty while doing the
 ##     BiSSE estimates.
@@ -44,39 +46,37 @@ tr <- to.genus.tree(phy, tip.matrix)
 ## There is the same number of species (and states) as the original phylogeny.
 ## They should match without problem.
 
-null.states <- data.frame(species = rownames(sim.null), sim.null, stringsAsFactors=FALSE)
-gen <- sapply(null.states[,1], function(x) strsplit(x, split = "_")[[1]][1], USE.NAMES = FALSE )
-null.states <- data.frame(genera = gen, null.states, stringsAsFactors=FALSE)
-rownames(null.states) <- NULL
+null.full.states <- data.frame(species = rownames(sim.full.null), sim.full.null, stringsAsFactors=FALSE)
+gen <- sapply(null.full.states[,1], function(x) strsplit(x, split = "_")[[1]][1], USE.NAMES = FALSE )
+null.full.states <- data.frame(genera = gen, null.full.states, stringsAsFactors=FALSE)
+rownames(null.full.states) <- NULL
+
+null.cons.states <- data.frame(species = rownames(sim.cons.null), sim.cons.null, stringsAsFactors=FALSE)
+gen <- sapply(null.cons.states[,1], function(x) strsplit(x, split = "_")[[1]][1], USE.NAMES = FALSE )
+null.cons.states <- data.frame(genera = gen, null.cons.states, stringsAsFactors=FALSE)
+rownames(null.cons.states) <- NULL
 
 ## Create the list of matrices to run BiSSE.
 ## Custom functions in 'functions/data-prepare.R'
-unres <- make.unres(null.states)
-st.bisse <- make.bisse.states(null.states)
+full.unres <- make.unres(null.full.states)
+full.st.bisse <- make.bisse.states(null.full.states)
+cons.unres <- make.unres(null.cons.states)
+cons.st.bisse <- make.bisse.states(null.cons.states)
 
-## Run MCMC BiSSE analysis with 20 first trees:
-## This analysis will take one week to complete. I am going to use more trees in a second
-##      batch of analyses if necessary.
-## WARNING: It depends on 'multicore' and takes time to run.
-## WARNING: It is set to use 20 cores.
-
-## library(multicore)
-## index <- 1:20
-## mcmc.no.effect <- mclapply(index, FUN = function(x) run.bisse(tree = tr[[x]], st = st.bisse[[x]]
-##           , unres = unres[[x]], tun.steps = 100, chain.steps = 10000, constrain = "FALSE"
-##           , flag = paste("no_effect_", x, sep="")), mc.cores = 20)
-
-## Save the full workspace to get all simulation results.
-## save.image(file = "no_effect_run_image.RData")
+save(full.unres, full.st.bisse, cons.st.bisse, cons.unres, file = "no_effect_sim_data.RData")
 
 ## Run a MLE BiSSE analysis with all the 100 trees:
-## This analysis will only produce a point estimate. However, the MCMC analysis of all trees
-##     will take much more time to complete.
+## Both with the full and cons simulated datasets.
+
 library(multicore)
 index <- 1:100
-mle.no.effect <- mclapply(index, FUN = function(x) run.bisse.mle(tree = tr[[x]], st = st.bisse[[x]]
-          , unres = unres[[x]], constrain = "FALSE", flag = paste("no_effect_mle", x, sep=""))
-          , mc.cores = 20)
 
-## Save simulation results:
-save(mle.no.effect, st.bisse, unres, tr, file = "no_effect_mle_results.RData")
+mle.cons.no.effect <- mclapply(index, FUN = function(x) run.bisse.mle(tree = tr[[x]], st = cons.st.bisse[[x]]
+          , unres = cons.unres[[x]], constrain = "FALSE", flag = paste("no_effect_mle", x, sep=""))
+          , mc.cores = 20)
+save(mle.cons.no.effect, file = "no_effect_cons_mle_results.RData")
+
+## mle.full.no.effect <- mclapply(index, FUN = function(x) run.bisse.mle(tree = tr[[x]], st = full.st.bisse[[x]]
+##           , unres = full.unres[[x]], constrain = "FALSE", flag = paste("no_effect_mle", x, sep=""))
+##           , mc.cores = 20)
+## save(mle.full.no.effect, file = "no_effect_full_mle_results.RData")
--- no_effect_results.R
+++ no_effect_results.R
@@ -5,8 +5,71 @@ library(geiger)
 source("./functions/data-prepare.R")
 source("./functions/analysis.R")
 
-###########################
-## Load the results:
+## Load the results of simulations:
+load("no-effect-results/no_effect_mle_results.RData")
+
+###################################
+## Check whether proportion of simulated states is representative of the empirical data.
+## The empirical proportion is 213 -> 1 and 381 -> 0.
+
+## Get proportion of states from sim datasets:
+head(unres[[1]])
+unres.st1 <- sapply(unres, function(x) sum(x[,3]) )
+vector.st1 <- sapply(st.bisse, function(x) sum(x, na.rm = TRUE) )
+st1 <- unres.st1 + vector.st1
+prop.st1 <- st1 / (213 + 381)
+prop.emp <- 213 / (213 + 381)
+
+hist(prop.st1, main = "States proportion", xlab = "St.1 / (St.0 + St.1)", breaks = 15, xlim = c(0,1))
+abline(v = prop.emp, lty = 2)
+## The proportion of contrasting species in the simulated datasets is equal or lower than the observed value.
+## This makes sense since the MK2 model is inadequade when there is a true effect of the state dependent
+##      diversification.
+## There is less contrasting species in the simulated datasets than what is observed, this means that
+##      the higher speciation rates would compensante this difference, since when in state 1 lineages
+##      accumulate faster.
+
+###############################################
+## Get the MLE of the parameter estimates to compare with the empirical results.
+
+mle.no.effect[[1]][[2]]$par
+mle.res <- lapply(mle.no.effect, function(x) x[[2]]$par)
+mle.res <- do.call(rbind, mle.res)
+
+net0 <- mle.res[,1] - mle.res[,3]
+net1 <- mle.res[,2] - mle.res[,4]
+
+## Get results from the empirical MCMC.
+## To compare with the MLE we will use the mean of the posterior distributions
+##     across the 100 trees.
+load("./mcmc_BiSSE_results/results_100_phylo_bisse.RData")
+rm(mcmc.onerate)
+
+mcmc.net0 <- sapply(1:100, function(x) mean( mcmc.tworate[[x]][[1]][,2] - mcmc.tworate[[x]][[1]][,4] ) )
+mcmc.net1 <- sapply(1:100, function(x) mean( mcmc.tworate[[x]][[1]][,3] - mcmc.tworate[[x]][[1]][,5] ) )
+
+## Plot the net diversification ML estimates across from the empirical and simulated results:
+mn <- min( c(net0, net1, mcmc.net0, mcmc.net1) )
+mx <- max( c(net0, net1, mcmc.net0, mcmc.net1) )
+plot(density(net0, from = mn, to = mx), xlim = c(mn,mx), ylim = c(0,2.5), col = "grey"
+   , xlab = "Net diversification (MLE)", lwd = 1.5, lty = 3
+   , main = "Distribution of ML estimates")
+lines(density(net1, from = mn, to = mx), col = "red", lwd = 1.5, lty = 3)
+lines(density(mcmc.net0, from = mn, to = mx), col = "grey", lwd = 1.5)
+lines(density(mcmc.net1, from = mn, to = mx), col = "red", lwd = 1.5)
+
+## Plot the magnitude of the difference between the net diversifications from the empirical data
+##      and the simulated.
+mn <- min( c(net1 - net0, mcmc.net1 - mcmc.net0) )
+mx <- max( c(net1 - net0, mcmc.net1 - mcmc.net0) )
+plot(density(net1 - net0, from = mn, to = mx), xlim = c(mn,mx), ylim = c(0,1.5), col = "blue"
+   , xlab = "Net diversification (MLE)", lwd = 1.5, main = "Magnitude of difference (net1 - net0)")
+lines(density(mcmc.net1 - mcmc.net0, from = mn, to = mx), col = "red", lwd = 1.5)
+
+###########################################
+## Now we are going to compare the results with MCMC posterior distributions for
+##        the 20 first set of trees.
+## load the results:
 
 load("no-effect-results/no_effect_results.RData")
 load("no-effect-results/no_effect_sim_states.RData")
@@ -97,18 +160,3 @@ get.overlap <- function(x,y){
     res <- length(left) / length(x)
     return(res)
 }
-
-## NEED TO DO THIS PART YET.
-## Check whether proportion of simulated states is representative of the empirical data.
-## The empirical proportion is 213 -> 1 and 381 -> 0.
-## Get proportion of states from sim datasets:
-pp <- lapply(1:ncol(sim.null), function(x) data.frame(table(sim.null[,x])) )
-pp.t <- sapply(pp, function(x) x[,2] )
-## Distribution of # of state 0 and state 1.
-## Uncomment to plot:
-pdf("Proportion_states.pdf")
-#plot(density(pp.t[2,] / (pp.t[2,] + pp.t[1,]) ), main = "States proportion - APO / (CR + APO)")
-hist(pp.t[2,] / (pp.t[2,] + pp.t[1,]), main = "States proportion", xlab = "APO / (CR + APO)"
-   , breaks = 10, xlim = c(0,1))
-abline(v = 213 / (381 + 213), lty = 2)
-dev.off()

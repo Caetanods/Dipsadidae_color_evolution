07deeb408f092bebb426d0318bae3170c0ebc7e7
Caetanods <caetanods1@gmail.com>
2015-04-08 11:39:11 -0700

    Make analysis with bounded MLE for 100 trees.

--- No_effect_analysis/no_effect_dataset_same_ratio_sim.R
+++ No_effect_analysis/no_effect_dataset_same_ratio_sim.R
@@ -25,15 +25,6 @@ prob <- 213 / (213 + 381)
 ## Use the 'to.make.rd.state' function to create randomly sampled datasets:
 sim.st <- lapply(1:100, function(x) to.make.rd.state(state, unres, freq0 = 1 - prob, freq1 = prob) )
 
-## Check the ratio of the simulated data:
-## pp.unres.st1 <- lapply(1:100, function(x) sum(sim.st[[x]][[2]][,3]) )
-## pp.st1 <- lapply(1:100, function(x) pp.unres.st1[[x]] + sum(sim.st[[x]][[1]], na.rm = TRUE) )
-## pp.st1 <- do.call(cbind, pp.st1)[1,]
-
-## hist(pp.st1 / (213 + 381), main = "States proportion", xlab = "St.1 / (St.0 + St.1)"
-##    , breaks = 15)
-## abline(v = prob, lty = 2)
-
 save(sim.st, file = "Same_ratio_sim_data.RData")
 
 ## Start the analysis:
@@ -42,15 +33,17 @@ save(sim.st, file = "Same_ratio_sim_data.RData")
 library(multicore)
 index <- 1:100
 
-sim.fit.cons <- mclapply(index, FUN = function(x) run.bisse.mle(tree = tree.genus[[1]]
+sim.fit.cons <- mclapply(index, FUN = function(x) run.bisse.bound.mle(tree = tree.genus[[1]]
                                   , st = sim.st[[x]][[1]], unres = sim.st[[x]][[2]]
+                                  , low = 0, up = 15
                                   , constrain = "TRUE", flag = paste("sim.fit.cons", x, sep=""))
                        , mc.cores = 20)
 save(sim.fit.cons, file = "sim.fit.cons.RData")
 rm(sim.fit.cons)
 
-sim.fit.full <- mclapply(index, FUN = function(x) run.bisse.mle(tree = tree.genus[[1]]
+sim.fit.full <- mclapply(index, FUN = function(x) run.bisse.bound.mle(tree = tree.genus[[1]]
                                   , st = sim.st[[x]][[1]], unres = sim.st[[x]][[2]]
+                                  , low = 0, up = 15
                                   , constrain = "FALSE", flag = paste("sim.fit.full", x, sep=""))
                        , mc.cores = 20)
 save(sim.fit.full, file = "sim.fit.full.RData")
@@ -60,10 +53,10 @@ rm(sim.fit.full)
 ## Now get the MLE for the empirical dataset:
 ## Create state vector for BiSSE:
 
-emp.fit.full <- run.bisse.mle(tree = tree.genus[[1]], st = state, unres = unres, constrain = "FALSE"
-                            , flag = "Empirical - full")
+emp.fit.full <- run.bisse.bound.mle(tree = tree.genus[[1]], st = state, unres = unres, constrain = "FALSE"
+                            , low = 0, up = 15, flag = "Empirical - full")
 save(emp.fit.full, file = "emp.fit.full.RData")
 
-emp.fit.cons <- run.bisse.mle(tree = tree.genus[[1]], st = state, unres = unres, constrain = "TRUE"
-                            , flag = "Empirical - cons")
+emp.fit.cons <- run.bisse.bound.mle(tree = tree.genus[[1]], st = state, unres = unres, constrain = "TRUE"
+                            , low = 0, up = 15, flag = "Empirical - cons")
 save(emp.fit.cons, file = "emp.fit.cons.RData")

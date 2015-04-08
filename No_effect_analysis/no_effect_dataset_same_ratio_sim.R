361121487a03cd5d39b050d365ed24c27de771e7
Caetanods <caetanods1@gmail.com>
2015-04-08 15:20:26 -0700

    Expand analysis for more 10 trees.

--- No_effect_analysis/no_effect_dataset_same_ratio_sim.R
+++ No_effect_analysis/no_effect_dataset_same_ratio_sim.R
@@ -1,7 +1,3 @@
-## There is a major problem in the previous simulations.
-## The datasets used a null model does not look like our data.
-## The ratios are very off. The empirical proportion is 213 -> 1 and 381 -> 0.
-
 ## Here I am going to sample from a binomial distribuition with probability
 ##      proportional to the diversity of the trait across the tree.
 ## The result should be datasets in average more close to the real data and
@@ -11,7 +7,10 @@
 ##      trees and calculate the MLE estimate of the constrained and full
 ##      models.
 
+## I am making parameter estimates for 100 simulated datasets for each empirical tree and 10 empirical trees.
+
 library(diversitree)
+library(multicore)
 source("../functions/analysis.R")
 
 ## Load BiSSE data:
@@ -24,39 +23,50 @@ prob <- 213 / (213 + 381)
 
 ## Use the 'to.make.rd.state' function to create randomly sampled datasets:
 sim.st <- lapply(1:100, function(x) to.make.rd.state(state, unres, freq0 = 1 - prob, freq1 = prob) )
-
 save(sim.st, file = "Same_ratio_sim_data.RData")
 
 ## Start the analysis:
-## Here I am running for just one tree. The tree number 1.
 
-library(multicore)
-index <- 1:100
+## Declare function to run simulations for one tree:
 
-sim.fit.cons <- mclapply(index, FUN = function(x) run.bisse.bound.mle(tree = tree.genus[[1]]
+run.lrt.sim <- function(ii){
+    ## ii; index of the phylogeny to be used.
+    ## All other objects are already in the workspace.
+    ## function return void. all objects are saved to the directory.
+
+    dd <- paste("./tree_genus_", ii, sep = "")
+    dir.create( dd )
+    
+    sim.fit.cons <- mclapply(1:100, FUN = function(x) run.bisse.bound.mle(tree = tree.genus[[ii]]
                                   , st = sim.st[[x]][[1]], unres = sim.st[[x]][[2]]
-                                  , low = 0, up = 15
-                                  , constrain = "TRUE", flag = paste("sim.fit.cons", x, sep=""))
-                       , mc.cores = 20)
-save(sim.fit.cons, file = "sim.fit.cons.RData")
-rm(sim.fit.cons)
-
-sim.fit.full <- mclapply(index, FUN = function(x) run.bisse.bound.mle(tree = tree.genus[[1]]
+                                  , low = 0, up = 20
+                                  , constrain = "TRUE", flag = paste("sim.fit.cons.", x,"phy.",ii, sep=""))
+                       , mc.cores = 22)
+    save(sim.fit.cons, file = paste(dd, "/", "sim.fit.cons.RData", sep = "") )
+    rm(sim.fit.cons)
+    
+    sim.fit.full <- mclapply(1:100, FUN = function(x) run.bisse.bound.mle(tree = tree.genus[[ii]]
                                   , st = sim.st[[x]][[1]], unres = sim.st[[x]][[2]]
-                                  , low = 0, up = 15
-                                  , constrain = "FALSE", flag = paste("sim.fit.full", x, sep=""))
-                       , mc.cores = 20)
-save(sim.fit.full, file = "sim.fit.full.RData")
-rm(sim.fit.full)
+                                  , low = 0, up = 20
+                                  , constrain = "FALSE", flag = paste("sim.fit.full", x,"phy.",ii, sep=""))
+                       , mc.cores = 22)
+    save(sim.fit.full, file = paste(dd, "/", "sim.fit.full.RData", sep = "") )
+    rm(sim.fit.full)
 
-#############################################
-## Now get the MLE for the empirical dataset:
-## Create state vector for BiSSE:
+    emp.fit.full <- run.bisse.bound.mle(tree = tree.genus[[ii]], st = state, unres = unres, constrain = "FALSE"
+                                      , low = 0, up = 20
+                                      , flag = paste("emp.fit.full.phy.", ii, sep="") )
+    save(emp.fit.full, file = paste(dd, "/", "emp.fit.full.RData", sep = "") )
+    rm(emp.fit.full)
+    
+    emp.fit.cons <- run.bisse.bound.mle(tree = tree.genus[[ii]], st = state, unres = unres, constrain = "TRUE"
+                                      , low = 0, up = 20
+                                      , flag = paste("emp.fit.cons.phy.", ii, sep="") )
+    save(emp.fit.cons, file = paste(dd, "/", "emp.fit.cons.RData", sep = "") )
+    rm(emp.fit.cons)
+}
 
-emp.fit.full <- run.bisse.bound.mle(tree = tree.genus[[1]], st = state, unres = unres, constrain = "FALSE"
-                            , low = 0, up = 15, flag = "Empirical - full")
-save(emp.fit.full, file = "emp.fit.full.RData")
+sapply(2:10, function(x) run.lrt.sim(x) )
 
-emp.fit.cons <- run.bisse.bound.mle(tree = tree.genus[[1]], st = state, unres = unres, constrain = "TRUE"
-                            , low = 0, up = 15, flag = "Empirical - cons")
-save(emp.fit.cons, file = "emp.fit.cons.RData")
+## The tree number 1 is already done in macumba in the following directory:
+## '/Documents/snake_manuscript/diversitree_analysis/mle_equal_prop_tree_gen_1'

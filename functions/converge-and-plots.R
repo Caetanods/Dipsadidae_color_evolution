9a3fb29dc55a3ed2ba07db5a7522f73476ba8264
Caetanods <caetanods1@gmail.com>
2015-10-21 15:45:26 -0700

    Delete plotting functions for the coda analysis.

--- functions/converge-and-plots.R
+++ functions/converge-and-plots.R
@@ -1,37 +1,3 @@
-to.mcmc.plot <- function(mcmc, run, dir, burn){
-    ## mcmc: The log from BiSSE.
-    ## run: String to identify the run.
-    ## dir: Path for the directory to save plots.
-    ## burn: Burnin for the diversitree profile plots.
-
-    dir.create(dir)
-    index <- round(seq(1, dim(mcmc)[1], length.out = 500))
-
-    ng <- paste(dir, "trace_", run,".pdf", sep="")
-    pdf(ng)
-    plot(mcmc$p[index], type = "l")
-    dev.off()
-
-    ## Taking out burn-in:
-    burn <- mcmc[(dim(mcmc)[1]*25/100):dim(mcmc)[1],]
-
-    lb <- paste(dir, "lambda_", run,".pdf", sep="")
-    mu <- paste(dir, "mu_", run,".pdf", sep="")
-    mk <- paste(dir, "markov_", run,".pdf", sep="")
-
-    pdf(file = lb)
-    profiles.plot(burn[c("lambda0")], col.line = c("yellow"))
-    dev.off()
-    
-    pdf(file = mu)
-    profiles.plot(burn[c("mu1")], col.line = c("yellow"))
-    dev.off()
-
-    pdf(file = mk)
-    profiles.plot(burn[c("q01","q10")], col.line = c("yellow","blue"), legend.pos = "topright")
-    dev.off()
-}
-
 to.heidel.diag <- function(bisse){
     ## bisse: output of the 'run.bisse' function.
     
